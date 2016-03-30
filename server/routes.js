5107b398b35dd64eb7f04b163b2eb04e1746ef96
Battulga Tsogtgerel <ts.battulga18@gmail.com>
2016-03-30 12:54:40 +0900

    Origin problems fixed

--- server/routes.js
+++ server/routes.js
@@ -12,7 +12,7 @@ exports['default'] = function(app, passport) {
 
 
   app.use(function(req, res, next){
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://52.25.241.31');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
