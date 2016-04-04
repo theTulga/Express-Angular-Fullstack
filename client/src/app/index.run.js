4ec29fe96e9d11fcb1f0bcd117d64eab7ef71788
Battulga Tsogtgerel <ts.battulga18@gmail.com>
2016-04-04 23:06:15 +0900

    sync

--- client/src/app/index.run.js
+++ client/src/app/index.run.js
@@ -8,8 +8,12 @@
   /** @ngInject */
   function runBlock($log, $rootScope, $state, redactorOptions, $cookies, config) {
 
-    redactorOptions.imageUpload = config.dev.host + '/image?XSRF-TOKEN=' + $cookies.get('XSRF-TOKEN');
-
+    if (config.env === 'dev'){
+      redactorOptions.imageUpload = config.dev.host + '/image?XSRF-TOKEN=' + $cookies.get('XSRF-TOKEN');
+    }
+    else {
+      redactorOptions.imageUpload = config.app.host + '/image?XSRF-TOKEN=' + $cookies.get('XSRF-TOKEN');
+    }
     $rootScope.$on('$stateChangeError', function() {
       $state.go('login');
     })
